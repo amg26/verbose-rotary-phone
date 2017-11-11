ed4f20581b15d3ddecf05bae54e9b94f883a3a7a
UristMcBaggins <shenk.philip@gmail.com>
2017-11-11 14:28:03 -0500

    Add JPanel & J

--- src/SimulationRenderer.java
+++ src/SimulationRenderer.java
@@ -1,14 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class SimulationRenderer extends JPanel {
     private Simulation simulation;
     private double[][] map;
+    private ArrayList<Entity> entities;
 
     public SimulationRenderer(Simulation simulation){
         this.simulation = simulation;
 
         map = simulation.getMap().getMap(); // lol
+        entities = simulation.getEntities();
 
         this.setPreferredSize(new Dimension(500, 500));
     }
@@ -27,6 +30,11 @@ public class SimulationRenderer extends JPanel {
                 g.setColor(c);
                 g.fillRect(i * 5, j * 5, 5, 5);
             }
+        }
+        g.setColor(Color.CYAN);
+        for(Entity e : entities){
+            g.fillOval((int)e.getX(), (int)e.getY(), 10, 10);
+
         }
     }
 }
