8edff5b917aa29b3fbf7d83c89cad6c013348ea8
Andrew Gagen <andrew.m.gagen@gmail.com>
2017-11-11 16:22:15 -0500

    Add map.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public  class Main {
     public static void main(String[] args) {
         Map m = new Map(50, 100);
-        System.out.println(m.getElevation(40, 60));
+        System.out.println(m.getElevation(3.5, 2.5));
 
-        Position p = new Position(40, 60);
+        Position p = new Position(7.3, 8.2);
         System.out.println(m.getElevation(p));
     }
 }
